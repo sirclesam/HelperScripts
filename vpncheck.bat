99760b7412edaa899a204f3523f444c895ea4d95
sirclesam <sirclesam@yahoo.com>
2015-06-09 21:52:16 -0700

    Changed to use pia status file, not look for openvpn.exe

--- vpncheck.bat
+++ vpncheck.bat
@@ -1,15 +1,13 @@
 @echo off
 
 :again
-	tasklist /FI "IMAGENAME eq openvpn.exe" 2>NUL | find /I /N "openvpn.exe">NUL
-	if "%ERRORLEVEL%" NEQ "0" (
-		echo Programm NOT is running
-		::taskkill /f /im uTorrent.exe
-		taskkill /f /im qbittorrent.exe
-		
-	) else (
-		echo VPN on, we good
-	)
-	echo waiting for 1sec
-	ping 1.1.1.1 -n 1 -w 500 > nul
+findstr  "CONNECTED" "F:\Program Files\pia_manager\data\status_file.txt" > nul
+if "%ERRORLEVEL%" NEQ "0" (
+	echo VPN NOT is connected
+	taskkill /f /im qbittorrent.exe		
+) else (
+	echo VPN on, we good
+)
+echo waiting for 1sec
+ping 1.1.1.1 -n 1 -w 500 > nul
 goto again
