734b23e04bf5b20574f9e87a7e381943d1cd8ce1
Sam <sirclesam@gmail.com>
2015-08-03 19:38:12 -0700

    updated to catch when program crashes and don't update status file.

--- vpncheck.bat
+++ vpncheck.bat
@@ -1,13 +1,24 @@
 @echo off
 
 :again
-findstr  "CONNECTED" "F:\Program Files\pia_manager\data\status_file.txt" > nul
+findstr  "CONNECTED" "C:\Program Files\pia_manager\data\status_file.txt" > nul
 if "%ERRORLEVEL%" NEQ "0" (
 	echo VPN NOT is connected
 	taskkill /f /im qbittorrent.exe		
 ) else (
-	echo VPN on, we good
+	echo File Status = CONNECTED
 )
+tasklist /FI "IMAGENAME eq openvpn.exe" 2>NUL | find /I /N "openvpn.exe">NUL
+if "%ERRORLEVEL%" NEQ "0" (
+	echo Programm NOT is running
+	::taskkill /f /im uTorrent.exe
+	taskkill /f /im qbittorrent.exe
+	
+) else (
+	echo OpenVPN Running
+)
+
 echo waiting for 1sec
 ping 1.1.1.1 -n 1 -w 500 > nul
+echo -------------------------
 goto again
